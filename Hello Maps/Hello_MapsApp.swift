5ee9c3b0c33e64be34297ca0e120da8bbd7fe1c7
MohamedGoush <0x1110111@gmail.com>
2023-10-30 17:29:20 +0530

    Initial Commit

---
+++ Hello Maps/Hello_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_MapsApp.swift
+//  Hello Maps
+//
+//  Created by python on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
